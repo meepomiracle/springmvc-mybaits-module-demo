30880c55707a173be48db4c3681ed0bbdafa4842
meepomiracle <53315557@qq.com>
2016-07-21 18:00:44 +0800

    first commit

---
+++ springmvc-module-demo-dao/src/main/java/com/jd/b2b/App.java
@@ -0,0 +1,13 @@
+package com.jd.b2b;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ springmvc-module-demo-utils/src/main/java/com/demo/commons/PropertiesUtil.java
@@ -0,0 +1,7 @@
+package com.demo.commons;
+
+/**
+ * Created by cdduanjianfeng on 2016/7/21.
+ */
+public class PropertiesUtil {
+}
